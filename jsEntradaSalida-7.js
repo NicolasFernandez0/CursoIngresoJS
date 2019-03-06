de221cf3dc41c1c62eed093a85a5fbd574b6be6e
NicolasFernandez0 <48266281+NicolasFernandez0@users.noreply.github.com>
2019-03-06 00:24:10 -0300

    Entrada y salida
    Ejercicios 1 al 10 (Completo).

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,72 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+	var numeroUno;
+	var numeroDos;
+	var suma;
+
+	numeroUno = document.getElementById('numeroUno').value; 
+	numeroDos = document.getElementById('numeroDos').value;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	suma = (numeroUno + numeroDos);
+
+	alert("La sumas es: " +suma);
+}
+
+function restar()
+{
+	var numeroUno;
+	var numeroDos;
+	var resta;
+
+	numeroUno = document.getElementById('numeroUno').value; 
+	numeroDos = document.getElementById('numeroDos').value;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	resta = (numeroUno - numeroDos);
+
+	alert("La sumas es: " +resta);
+}
+
+function multiplicar()
+{ 
+	var numeroUno;
+	var numeroDos;
+	var multiplicacion;
+
+	numeroUno = document.getElementById('numeroUno').value; 
+	numeroDos = document.getElementById('numeroDos').value;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	multiplicacion = (numeroUno * numeroDos);
+
+	alert("La multiplicacion es: " +multiplicacion);
+}
+
+function dividir()
+{
+	var numeroUno;
+	var numeroDos;
+	var division;
+
+	numeroUno = document.getElementById('numeroUno').value; 
+	numeroDos = document.getElementById('numeroDos').value;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	division = (numeroUno / numeroDos);
+
+	alert("La division es: " +division);
+}
+
